cebb4eac7397291b33482c94150189d00a42b942
Jim Posen <jim.posen@gmail.com>
2019-11-09 22:54:04 +0100

    Implement basic matrix operations including inversion.

--- ring-signatures/src/matrix.rs
+++ ring-signatures/src/matrix.rs
@@ -1,6 +1,8 @@
 use curve25519_dalek::scalar::Scalar;
-use std::ops::{Index, IndexMut};
+use std::fmt;
+use std::ops::{Index, IndexMut, Mul};
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MatrixNN {
 	n: usize,
 	inner: Vec<Scalar>,
@@ -21,13 +23,21 @@ impl MatrixNN {
 		}
 	}
 
+	pub fn identity(n: usize) -> MatrixNN {
+		let mut mat = Self::zeros(n);
+		for i in 0..n {
+			mat[(i, i)] = Scalar::one();
+		}
+		mat
+	}
+
 	pub fn vandermonde(elements: &[Scalar]) -> MatrixNN {
 		let n = elements.len();
 		let mut mat = Self::zeros(n);
 		for i in 0..n {
 			mat[(i, 0)] = Scalar::one();
 			for j in 1..n {
-				mat[(i, j)] = mat[(i, j)] * elements[i];
+				mat[(i, j)] = mat[(i, j - 1)] * elements[i];
 			}
 		}
 		mat
@@ -39,6 +49,45 @@ impl MatrixNN {
 
 	pub fn get_mut(&mut self, i: usize, j: usize) -> Option<&mut Scalar> {
 		self.inner.get_mut(i * self.n + j)
+	}
+
+	pub fn inverse(mut self) -> Option<MatrixNN> {
+		let n = self.n;
+		let mut inv = MatrixNN::identity(n);
+
+		for i in 0..n {
+			let x = self[(i, i)];
+			if x == Scalar::zero() {
+				return None
+			}
+			let x_inv = x.invert();
+			self.scale_row(i, &x_inv);
+			inv.scale_row(i, &x_inv);
+
+			for ii in 0..n {
+				if i != ii {
+					let scalar = -self[(ii, i)];
+					self.add_rows(ii, i, &scalar, i);
+					inv.add_rows(ii, i, &scalar, 0);
+				}
+			}
+		}
+
+		assert_eq!(self, MatrixNN::identity(n));
+		Some(inv)
+	}
+
+	fn scale_row(&mut self, i: usize, x: &Scalar) {
+		for j in 0..self.n {
+			self[(i, j)] *= x;
+		}
+	}
+
+	fn add_rows(&mut self, i1: usize, i2: usize, x: &Scalar, start_col: usize) {
+		for j in start_col..self.n {
+			let y = self[(i2, j)] * x;
+			self[(i1, j)] += y;
+		}
 	}
 }
 
@@ -56,15 +105,42 @@ impl IndexMut<(usize, usize)> for MatrixNN {
 	}
 }
 
+impl Mul for MatrixNN {
+	type Output = MatrixNN;
+
+	fn mul(self, rhs: Self) -> Self::Output {
+		Mul::mul(&self, &rhs)
+	}
+}
+
+impl Mul for &MatrixNN {
+	type Output = MatrixNN;
+
+	fn mul(self, rhs: Self) -> Self::Output {
+		let n = self.n;
+		let mut out = MatrixNN::zeros(n);
+		for i in 0..n {
+			for j in 0..n {
+				for k in 0..n {
+					out[(i, j)] += self[(i, k)] * rhs[(k, j)];
+				}
+			}
+		}
+		out
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
-	fn test_vandermonde() {
-		let elements = (0u32..20u32)
+	fn test_vandermonde_inverse() {
+		let elements = (1u32..=25u32)
 			.map(Scalar::from)
 			.collect::<Vec<_>>();
-		MatrixNN::vandermonde(&elements);
+		let mat = MatrixNN::vandermonde(&elements);
+		let mat_inv = mat.clone().inverse().unwrap();
+		assert_eq!(mat * mat_inv, MatrixNN::identity(25));
 	}
 }
